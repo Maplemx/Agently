3b955c3f3c0f0688f283dd90118ba486096f9b3c
Maplemx <maplemx@gmail.com>
2023-11-01 13:11:12 +0800

    new feature: 1. Agent status auto save and manual save; 2. Better debug mode which show request data and realtime response clearly; update: 1. Add refresh_plugins() method to refresh plugins after install new plugins; bug fix: 1. add Exception() to some exception raise to ensure error report correctly.

--- src/python/v3/agent/Agent.py
+++ src/python/v3/agent/Agent.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 import asyncio
-from ..request import Request
+from ..Request import Request
 from ..utils import RuntimeCtx, StorageDelegate, PluginManager, AliasManager, IdGenerator, to_json_desc, find_json, check_version
 
 class Agent(object):
@@ -9,9 +9,10 @@ class Agent(object):
         self,
         *,
         agent_id: str=None,
+        auto_save: bool=False,
         parent_agent_runtime_ctx: object,
         global_storage: object,
-        parent_plugin_manager: object
+        parent_plugin_manager: object,
     ):
         # Integrate
         self.global_storage = global_storage
@@ -33,23 +34,30 @@ class Agent(object):
             db_name = self.agent_id,
             plugin_manager = self.plugin_manager,
         )
+        # Load Saved agent_runtime_ctx
+        self.agent_runtime_ctx.update_by_dict(self.agent_storage.table("agent_runtime_ctx").get())
+        # Set Agent Auto Save Setting
+        self.agent_runtime_ctx.set("agent_auto_save", auto_save)
         # Version Check In Debug Model
         if self.plugin_manager.get_settings("is_debug"):
             check_version_record = self.global_storage.get("agently", "check_version_record")
             today = str(datetime.date.today())
             if check_version_record != today:
-                check_version()
-                self.global_storage.set("agently", "check_version_record", today)
+                check_version(self.global_storage, today)
         # Agent Request Prefix & Suffix
         self.agent_request_prefix = []
         self.agent_request_suffix = []
         # Register Default Request Alias to Agent
         self.request._register_default_alias(self.alias_manager)
+        # Install Agent Components
+        self.refresh_plugins()
+
+    def refresh_plugins():
         # Agent Components
         agent_components = self.plugin_manager.get("agent_component")
         component_toggles = self.plugin_manager.get_settings("component_toggles")
         for agent_component_name, AgentComponentClass in agent_components.items():
-            # Skip component_toggles.Those Be Toggled Off
+            # Skip component_toggles Those Be Toggled Off
             if agent_component_name in component_toggles and component_toggles[agent_component_name] == False:
                     continue
             # Attach Component
@@ -76,8 +84,23 @@ class Agent(object):
                         return_value = alias_info["return_value"] if "return_value" in alias_info else False,
                     )
 
+    def toggle_auto_save(self, is_enabled: bool):
+        self.agent_runtime_ctx.set("agent_auto_save", is_enabled)
+        return self
+
+    def save(self):
+        self.agent_storage.table("agent_runtime_ctx").update_by_dict(self.agent_runtime_ctx.get()).save()
+        return self
+
+    def set_settings(self, settings_key: str, settings_value: any):
+        self.plugin_manager.set_settings(settings_key, settings_value)
+        return self
+
     async def async_start(self):
         is_debug = self.plugin_manager.get_settings("is_debug")
+        # Auto Save Agent runtime_ctx
+        if self.agent_runtime_ctx.get("agent_auto_save") ==  True:
+            self.save()
         # Call Prefix Funcs to Prepare Prefix Data(From agent_runtime_ctx To request_runtime_ctx)
         for prefix_func in self.agent_request_prefix:
             prefix_data = await prefix_func() if asyncio.iscoroutinefunction(prefix_func) else prefix_func()
@@ -85,7 +108,7 @@ class Agent(object):
                 if isinstance(prefix_data, tuple) and isinstance(prefix_data[0], str) and prefix_data[1] != None:
                     self.request.request_runtime_ctx.update(prefix_data[0], prefix_data[1])
                 else:
-                    raise("[Agent Component] Prefix return data error: only accept None or Tuple('<Request Namespace>', <Update Dict>)")
+                    raise Exception("[Agent Component] Prefix return data error: only accept None or Tuple('<Request Namespace>', <Update Dict>)")
 
         # Request
         event_generator = self.request.get_event_generator()
@@ -105,11 +128,11 @@ class Agent(object):
                 print(response["data"], end="")
             await call_request_suffix(response)
             if response["event"] == "response:done":
-                if is_debug:
-                    print("\n--------------------------\n")
-                    print("[Final Response]\n", response["data"], "\n--------------------------\n")
                 if self.request.response_cache["reply"] == None:
                     self.request.response_cache["reply"] = response["data"]
+                if is_debug:
+                    print("\n--------------------------\n")
+                    print("[Final Reply]\n", self.request.response_cache["reply"], "\n--------------------------\n")
 
         await call_request_suffix({ "event": "response:finally", "data": self.request.response_cache })
 
@@ -117,6 +140,9 @@ class Agent(object):
         if self.request.response_cache["type"] == "JSON":
             try:
                 self.request.response_cache["reply"] = json.loads(find_json(self.request.response_cache["reply"]))
+                if is_debug:
+                    print("[Parse JSON to Dict] Done")
+                    print("\n--------------------------\n")
             except json.JSONDecodeError as e:
                 try:
                     fixed_result = await self.request\
@@ -130,8 +156,11 @@ class Agent(object):
                         .output('Fixed JSON String can be parsed by Python only without explanation and decoration.')\
                         .get_result_async()
                     self.request.response_cache["reply"] = json.loads(find_json(fixed_result))
+                    if is_debug:
+                        print("[Parse JSON to Dict] Done")
+                        print("\n--------------------------\n")
                 except Exception as e:
-                    raise(f"[Agent Request] Error still occured when try to fix JSON decode error: { str(e) }")
+                    raise Exception(f"[Agent Request] Error still occured when try to fix JSON decode error: { str(e) }")
 
         self.request_runtime_ctx.empty()
         return self.request.response_cache["reply"]
