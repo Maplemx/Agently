a5684778dc2748cfe78e1ef8509b0cdc9d16c13f
Maplemx <Maplemx@gmail.com>
2023-11-14 13:56:22 +0800

    new feature: support GPT-4-vision use example: agent.input("what is this?").file("<image url>").start("vision")

--- src/plugins/request/OpenAI.py
+++ src/plugins/request/OpenAI.py
@@ -11,6 +11,9 @@ class OpenAI(RequestABC):
         self.assistant_id = None
         self.model_name = "OpenAI"
         self.model_settings = RuntimeCtxNamespace(f"model.{ self.model_name }", self.request.settings)
+        self.request_type = self.request.request_runtime_ctx.get("request_type", "chat")
+        if self.request_type == None:
+            self.request_type = "chat"
 
     def _create_client(self):
         client_params = {}
@@ -56,11 +59,14 @@ class OpenAI(RequestABC):
         prompt_information_data = self.request.request_runtime_ctx.get_trace_back("prompt.information")
         prompt_instruction_data = self.request.request_runtime_ctx.get_trace_back("prompt.instruction")
         prompt_output_data = self.request.request_runtime_ctx.get_trace_back("prompt.output")
+        # - files
+        files_data = self.request.request_runtime_ctx.get_trace_back("prompt.files")
         # --- only input
         if not prompt_input_data and not prompt_information_data and not prompt_instruction_data and not prompt_output_data:
             raise Exception("[Request] Missing 'prompt.input', 'prompt.information', 'prompt.instruction', 'prompt.output' in request_runtime_ctx. At least set value to one of them.")
+        prompt_text = ""
         if prompt_input_data and not prompt_information_data and not prompt_instruction_data and not prompt_output_data:
-            request_messages.append({ "role": "user", "content": to_instruction(prompt_input_data) })
+            prompt_text = to_instruction(prompt_input_data)
         # --- construct prompt
         else:
             prompt_dict = {}
@@ -79,40 +85,57 @@ class OpenAI(RequestABC):
                     self.request.request_runtime_ctx.set("response:type", "JSON")
                 else:
                     prompt_dict["[OUTPUT REQUIERMENT]"] = str(prompt_output_data)
-            request_messages.append({ "role": "user", "content": to_prompt_structure(prompt_dict, end="[OUTPUT]:\n") })
-        return request_messages
-
-    def generate_request_data_for_gpt(self):
-        options = self.model_settings.get_trace_back("options", {})
-        if "model" not in options:
-            options.update({ "model": "gpt-3.5-turbo" })
-        return {
-            "stream": True,
-            "messages": self.construct_request_messages(),
-            **options
-        }
-
-    def generate_request_data_for_assistant(self):
-        options = self.model_settings.get_trace_back("options", {})
-        if "model" not in options:
-            options.update({ "model": "gpt-3.5-turbo" })
-        return {
-            "stream": True,
-            "messages": self.construct_request_messages(),
-            "options": options,
-        }
+            prompt_text = to_prompt_structure(prompt_dict, end="[OUTPUT]:\n")
+        # --- for vision model
+        if self.request_type == "vision" and files_data:
+            content = []
+            content.append({ "type": "text", "text": prompt_text })
+            for image_content in files_data:
+                if isinstance(image_content, str):
+                    content.append({ "type": "image_url", "image_url": { "url": image_content } })
+                elif isinstance(image_content, dict):
+                    content.append({ "type": "image_url", "image_url": image_content })
+                else:
+                    raise Exception(f"[Request] OpenAI vision model .files() can only accept string or dict.")
+            request_messages.append({ "role": "user", "content": content })
+        else:
+            request_messages.append({ "role": "user", "content": prompt_text })
+        return request_messages                
 
     def generate_request_data(self):
         self.use_assistant = self.model_settings.get_trace_back("use_assistant", False)
-        if self.use_assistant:
+        if self.request_type == "chat" and self.use_assistant:
             assistant_id = self.model_settings.get_trace_back("assistant_id")
             if not assistant_id:
                 raise Exception("[Request] OpenAI require 'assistant_id' when 'use_assistant' is True. Use agent.OpenAIAssistant.update() to create an assistant if you don't have one.")
             else:
                 self.assistant_id = assistant_id
-            return self.generate_request_data_for_assistant()
-        else:
-            return self.generate_request_data_for_gpt()
+            options = self.model_settings.get_trace_back("options", {})
+            if "model" not in options:
+                options.update({ "model": "gpt-3.5-turbo" })
+            return {
+                "stream": True,
+                "messages": self.construct_request_messages(),
+                "options": options,
+            }
+        elif self.request_type == "chat":
+            options = self.model_settings.get_trace_back("options", {})
+            if "model" not in options:
+                options.update({ "model": "gpt-3.5-turbo" })
+            return {
+                "stream": True,
+                "messages": self.construct_request_messages(),
+                **options
+            }
+        elif self.request_type == "vision":
+            options = self.model_settings.get_trace_back("options", {})
+            if "model" not in options:
+                options.update({ "model": "gpt-4-vision-preview" })
+            return {
+                "stream": True,
+                "messages": self.construct_request_messages(),
+                **options
+            }
 
     def request_assiatant(self, request_data: dict):
         client = self._create_client()
@@ -165,16 +188,26 @@ class OpenAI(RequestABC):
         )
         return stream
 
+    def request_vision(self, request_data: dict):
+        client = self._create_client()
+        request_data["max_tokens"] = 4096
+        stream = client.chat.completions.create(
+            **request_data
+        )
+        return stream
+
     def request_model(self, request_data: dict):
-        if self.use_assistant:
+        if self.use_assistant and self.request_type == "chat":
             return self.request_assiatant(request_data)
-        else:
+        elif self.request_type == "chat":
             return self.request_gpt(request_data)
+        elif self.request_type == "vision":
+            return self.request_vision(request_data)
 
-    def broadcast_response_from_assistant(self, response_generator):
-        yield({ "event": "response:done", "data": response_generator })
+    def broadcast_response_without_streaming(self, response):
+        yield({ "event": "response:done", "data": response })
 
-    def broadcast_response_from_gpt(self, response_generator):
+    def broadcast_response_with_streaming(self, response_generator):
         response_message = {}
         for part in response_generator:
             delta = dict(part.choices[0].delta)
@@ -189,10 +222,12 @@ class OpenAI(RequestABC):
         yield({ "event": "response:done", "data": response_message["content"] })
 
     def broadcast_response(self, response_generator):
-        if self.use_assistant:
-            return self.broadcast_response_from_assistant(response_generator)
-        else:
-            return self.broadcast_response_from_gpt(response_generator)
+        if self.use_assistant and self.request_type == "chat":
+            return self.broadcast_response_without_streaming(response_generator)
+        elif self.request_type == "chat":
+            return self.broadcast_response_with_streaming(response_generator)
+        elif self.request_type == "vision":
+            return self.broadcast_response_with_streaming(response_generator)
 
     def export(self):
         return {
