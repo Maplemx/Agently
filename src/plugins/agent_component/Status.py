9e8de41057749d7c91377816669b2a7d68ec9272
Maplemx <Maplemx@gmail.com>
2023-11-24 23:29:17 +0800

    update: small optimization 1. change "component_settings" to "plugin_settings" 2. delete prefix component toggle judgement logic 3. set "default" as default value of .use_global_status(namespace_name) instead of None

--- src/plugins/agent_component/Status.py
+++ src/plugins/agent_component/Status.py
@@ -7,7 +7,7 @@ class Status(ComponentABC):
         self.status_runtime_ctx = RuntimeCtxNamespace("status", self.agent.agent_runtime_ctx)
         self.status_mapping_runtime_ctx = RuntimeCtxNamespace("status_mapping", self.agent.agent_runtime_ctx)
         self.status_storage = self.agent.global_storage.table("status")
-        self.settings = RuntimeCtxNamespace("component_settings.Status", self.agent.settings)
+        self.settings = RuntimeCtxNamespace("plugin_settings.agent_component.Status", self.agent.settings)
         self.global_status_namespace = None
         if self.settings.get_trace_back("auto"):
             self.load()
@@ -16,11 +16,11 @@ class Status(ComponentABC):
         self.status_runtime_ctx.set(key, value)
         return self.agent
 
-    def use_global_status(self, namespace_name: str=None):
-        self.global_status_namespace = namespace_name if namespace_name else "default"
+    def use_global_status(self, namespace_name: str="default"):
+        self.global_status_namespace = namespace_name
         return self.agent
 
-    def append_mapping(self, status_key: str, status_value, alias_name: str, *args, **kwargs):
+    def append_mapping(self, status_key: str, status_value: str, alias_name: str, *args, **kwargs):
         self.status_mapping_runtime_ctx.append(
             f"{ status_key }.{ status_value }",
             {
@@ -42,28 +42,24 @@ class Status(ComponentABC):
         return self.agent
 
     def _early(self):
-        component_toggle = self.agent.settings.get_trace_back("component_toggles.Status")
-        if component_toggle:
-            agent_status = self.status_runtime_ctx.get()
-            if agent_status:
-                # get mappings
-                global_status_mappings_dict = self.agent.global_storage.table(f"status_mapping.{ self.global_status_namespace }").get() if self.global_status_namespace != None else {}
-                agent_status_mappings_dict = self.status_mapping_runtime_ctx.get()
-                if agent_status_mappings_dict == None:
-                    agent_status_mappings_dict = {}
-                for status_key, status_value in self.status_runtime_ctx.get().items():
-                    # handle global mappings first
-                    if status_key in global_status_mappings_dict and status_value in global_status_mappings_dict[status_key]:
-                        for global_status_mapping in global_status_mappings_dict[status_key][status_value]:
-                            getattr(self.agent, global_status_mapping["alias_name"])(*global_status_mapping["args"], **global_status_mapping["kwargs"])
-                    # handle agent mappings
-                    if status_key in agent_status_mappings_dict and status_value in agent_status_mappings_dict[status_key]:
-                        for agent_status_mapping in agent_status_mappings_dict[status_key][status_value]:
-                            getattr(self.agent, agent_status_mapping["alias_name"])(*agent_status_mapping["args"], **agent_status_mapping["kwargs"])
-            return None
-        else:
-            return None
-
+        agent_status = self.status_runtime_ctx.get()
+        if agent_status:
+            # get mappings
+            global_status_mappings_dict = self.agent.global_storage.table(f"status_mapping.{ self.global_status_namespace }").get() if self.global_status_namespace != None else {}
+            agent_status_mappings_dict = self.status_mapping_runtime_ctx.get()
+            if agent_status_mappings_dict == None:
+                agent_status_mappings_dict = {}
+            for status_key, status_value in self.status_runtime_ctx.get().items():
+                # handle global mappings first
+                if status_key in global_status_mappings_dict and status_value in global_status_mappings_dict[status_key]:
+                    for global_status_mapping in global_status_mappings_dict[status_key][status_value]:
+                        getattr(self.agent, global_status_mapping["alias_name"])(*global_status_mapping["args"], **global_status_mapping["kwargs"])
+                # handle agent mappings
+                if status_key in agent_status_mappings_dict and status_value in agent_status_mappings_dict[status_key]:
+                    for agent_status_mapping in agent_status_mappings_dict[status_key][status_value]:
+                        getattr(self.agent, agent_status_mapping["alias_name"])(*agent_status_mapping["args"], **agent_status_mapping["kwargs"])
+        return None
+        
     def export(self):
         return {
             "early": self._early,
