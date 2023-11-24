b924dcd9661d2e090f56a0e33ff16d0450539cb6
Maplemx <Maplemx@gmail.com>
2023-11-24 23:24:00 +0800

    update: seperate agent component "Role" and "UserInfo"

--- src/plugins/agent_component/Role.py
+++ src/plugins/agent_component/Role.py
@@ -4,51 +4,39 @@ from Agently.utils import RuntimeCtxNamespace
 class Role(ComponentABC):
     def __init__(self, agent: object):
         self.agent = agent
-        self.user_info_runtime_ctx = RuntimeCtxNamespace("user_info", self.agent.agent_runtime_ctx)
         self.role_runtime_ctx = RuntimeCtxNamespace("role", self.agent.agent_runtime_ctx)
         self.role_storage = self.agent.global_storage.table("role")
 
-    def __get_runtime_ctx(self, target: str):
-        if target == "role":
-            return self.role_runtime_ctx
-        elif target == "user_info":
-            return self.user_info_runtime_ctx
-
-    def set_name(self, name: str, *, target: str):
-        runtime_ctx = self.__get_runtime_ctx(target)
-        runtime_ctx.set("NAME", name)
+    def set_name(self, name: str):
+        self.role_runtime_ctx.set("NAME", name)
         return self.agent
 
-    def set(self, key: any, value: any=None, *, target: str):
-        runtime_ctx = self.__get_runtime_ctx(target)
+    def set(self, key: any, value: any=None):
         if value is not None:
-            runtime_ctx.set(key, value)
+            self.role_runtime_ctx.set(key, value)
         else:
-            runtime_ctx.set("DESC", key)
+            self.role_runtime_ctx.set("DESC", key)
         return self.agent
 
-    def update(self, key: any, value: any=None, *, target: str):
-        runtime_ctx = self.__get_runtime_ctx(target)
+    def update(self, key: any, value: any=None):
         if value is not None:
-            runtime_ctx.update(key, value)
+            self.role_runtime_ctx.update(key, value)
         else:
-            runtime_ctx.update("DESC", key)
+            self.role_runtime_ctx.update("DESC", key)
         return self.agent        
 
-    def append(self, key: any, value: any=None, *, target: str):
-        runtime_ctx = self.__get_runtime_ctx(target)
+    def append(self, key: any, value: any=None):
         if value is not None:
-            runtime_ctx.append(key, value)
+            self.role_runtime_ctx.append(key, value)
         else:
-            runtime_ctx.append("DESC", key)
+            self.role_runtime_ctx.append("DESC", key)
         return self.agent
 
-    def extend(self, key: any, value: any=None, *, target: str):
-        runtime_ctx = self.__get_runtime_ctx(target)
+    def extend(self, key: any, value: any=None):
         if value is not None:
-            runtime_ctx.extend(key, value)
+            self.role_runtime_ctx.extend(key, value)
         else:
-            runtime_ctx.extend("DESC", key)
+            self.role_runtime_ctx.extend("DESC", key)
         return self.agent
 
     def save(self, role_name: str=None):
@@ -60,7 +48,7 @@ class Role(ComponentABC):
                 .save()
             return self.agent
         else:
-            raise Exception("[Agent Component: Role] Role attr 'NAME' must be stated before save.")
+            raise Exception("[Agent Component: Role] Role attr 'NAME' must be stated before save. Use .set_role_name() to specific that.")
 
     def load(self, role_name: str):
         role_data = self.role_storage.get(role_name)
@@ -69,30 +57,20 @@ class Role(ComponentABC):
         return self.agent
 
     def _prefix(self):
-        component_toggle = self.agent.settings.get_trace_back("component_toggles.Role")
-        if component_toggle:
-            return {
-                "role": self.role_runtime_ctx.get(),
-                "user_info": self.user_info_runtime_ctx.get(),
-            }
-        else:
-            return None
+        return {
+            "role": self.role_runtime_ctx.get(),
+        }
 
     def export(self):
         return {
             "prefix": self._prefix,
             "suffix": None,
             "alias": {
-                "set_role_name": { "func": lambda name: self.set_name(name, target="role") },
-                "set_role": { "func": lambda key, value=None: self.set(key, value, target="role") },
-                "update_role": { "func": lambda key, value=None: self.update(key, value, target="role") },
-                "append_role": { "func": lambda key, value=None: self.append(key, value, target="role") },
-                "extend_role": { "func": lambda key, value=None: self.extend(key, value, target="role") },
-                "set_user_name": { "func": lambda name: self.set_name(name, target="user_info") },
-                "set_user_info": { "func": lambda key, value=None: self.set(key, value, target="user_info") },
-                "update_user_info": { "func": lambda key, value=None: self.update(key, value, target="user_info") },
-                "append_user_info": { "func": lambda key, value=None: self.append(key, value, target="user_info") },
-                "extend_user_info": { "func": lambda key, value=None: self.extend(key, value, target="user_info") },
+                "set_role_name": { "func": self.set_name },
+                "set_role": { "func": self.set },
+                "update_role": { "func": self.update },
+                "append_role": { "func": self.append },
+                "extend_role": { "func": self.extend },
                 "save_role": { "func": self.save },
                 "load_role": { "func": self.load },
             },
---
+++ src/plugins/agent_component/UserInfo.py
@@ -0,0 +1,81 @@
+from .utils import ComponentABC
+from Agently.utils import RuntimeCtxNamespace
+
+class UserInfo(ComponentABC):
+    def __init__(self, agent: object):
+        self.agent = agent
+        self.user_info_runtime_ctx = RuntimeCtxNamespace("user_info", self.agent.agent_runtime_ctx)
+        self.user_info_storage = self.agent.global_storage.table("user_info")
+
+    def set_name(self, name: str):
+        user_info_runtime_ctx = self.__get_user_info_runtime_ctx(target)
+        user_info_runtime_ctx.set("NAME", name)
+        return self.agent
+
+    def set(self, key: any, value: any=None):
+        if value is not None:
+            user_info_runtime_ctx.set(key, value)
+        else:
+            user_info_runtime_ctx.set("DESC", key)
+        return self.agent
+
+    def update(self, key: any, value: any=None):
+        if value is not None:
+            user_info_runtime_ctx.update(key, value)
+        else:
+            user_info_runtime_ctx.update("DESC", key)
+        return self.agent        
+
+    def append(self, key: any, value: any=None):
+        if value is not None:
+            user_info_runtime_ctx.append(key, value)
+        else:
+            user_info_runtime_ctx.append("DESC", key)
+        return self.agent
+
+    def extend(self, key: any, value: any=None):
+        if value is not None:
+            user_info_runtime_ctx.extend(key, value)
+        else:
+            user_info_runtime_ctx.extend("DESC", key)
+        return self.agent
+
+    def save(self, role_name: str=None):
+        if user_info_name == None:
+            user_info_name = self.user_info_runtime_ctx.get("NAME")
+        if user_info_name != None and user_info_name != "":
+            self.user_info_storage\
+                .set(user_info_name, self.user_info_runtime_ctx.get())\
+                .save()
+            return self.agent
+        else:
+            raise Exception("[Agent Component: UserInfo] UserInfo attr 'NAME' must be stated before save. Use .set_user_name() to specific that.")
+
+    def load(self, role_name: str):
+        user_info_data = self.user_info_storage.get(role_name)
+        for key, value in user_info_data.items():
+            self.user_info_runtime_ctx.update(key, value)
+        return self.agent
+
+    def _prefix(self):
+        return {
+            "user_info": self.user_info_runtime_ctx.get(),
+        }
+
+    def export(self):
+        return {
+            "prefix": self._prefix,
+            "suffix": None,
+            "alias": {
+                "set_user_name": { "func": self.set_name },
+                "set_user_info": { "func": self.set },
+                "update_user_info": { "func": self.update },
+                "append_user_info": { "func": self.append },
+                "extend_user_info": { "func": self.extend },
+                "save_user_info": { "func": self.save },
+                "load_user_info": { "func": self.load },
+            },
+        }
+
+def export():
+    return ("UserInfo", UserInfo)
