2e31bbe324e40d37eb647e5b6a65bfb471564a10
Maplemx <Maplemx@gmail.com>
2024-01-06 00:13:13 +0800

    update: add .to_prompt_structure

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,6 +5,6 @@ from .AliasManager import AliasManager
 from .ToolManager import ToolManager
 from .IdGenerator import IdGenerator
 from .DataOps import DataOps, NamespaceOps
-from .transform import to_json_desc, to_instruction, find_all_jsons, find_json
+from .transform import to_prompt_structure, to_json_desc, to_instruction, find_all_jsons, find_json
 from .check_version import check_version
 from .load_json import load_json
