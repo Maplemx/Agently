dc81de098cc1a84385e3da25cac393e28ee461c3
Maplemx <Maplemx@gmail.com>
2024-02-02 11:10:03 +0800

    update: add strict orders mode to help chat history management for models require strict U-A-U-A orders

--- src/plugins/agent_component/Session.py
+++ src/plugins/agent_component/Session.py
@@ -1,20 +1,27 @@
 import uuid
 import yaml
 from .utils import ComponentABC
+from Agently.utils import RuntimeCtxNamespace
 
 class Session(ComponentABC):
     def __init__(self, agent):
         self.agent = agent
+        self.settings = RuntimeCtxNamespace("plugin_settings.agent_component.Session", self.agent.settings)
         self.current_session_id = None
         self.full_chat_history = []
         self.recent_chat_history = []
+        self.abstract = RuntimeCtxNamespace("abstract", self.agent.agent_runtime_ctx)
 
     def toggle_auto_save(self, is_enabled: bool):
-        self.agent.settings.set("plugin_settings.agent_component.Session.auto_save", is_enabled)
+        self.settings.set("auto_save", is_enabled)
         return self.agent
 
     def set_max_length(self, max_length: int):
-        self.agent.settings.set("plugin_settings.agent_component.Session.max_length", max_length)
+        self.settings.set("max_length", max_length)
+        return self.agent
+
+    def set_strict_orders(self, is_strict_orders: bool):
+        self.settings.set("strict_orders", is_strict_orders)
         return self.agent
 
     def active(self, session_id: str=None):
@@ -35,19 +42,64 @@ class Session(ComponentABC):
         return self.agent
 
     def stop(self):
-        if self.agent.settings.get_trace_back("plugin_settings.agent_component.Session.auto_save"):
+        if self.settings.get_trace_back("auto_save"):
             self.agent.agent_storage.table("chat_history").set(self.current_session_id, self.full_chat_history).save()
         self.current_session_id = None
         self.full_chat_history = []
         self.recent_chat_history = []
         return self.agent
 
-    def shorten_chat_history(self):
-        if len(str(self.recent_chat_history)) > self.agent.settings.get_trace_back("plugin_settings.agent_component.Session.max_length"):
-            self.recent_chat_history = self.recent_chat_history[1:]
-            self.shorten_chat_history()
+    def __get_shorten_chat_history(self, current_chat_history: list, max_length: int):
+        if len(str(current_chat_history)) > max_length:
+            self.__get_shorten_chat_history(current_chat_history[1:], max_length)
         else:
-            return self.agent
+            return current_chat_history
+
+    def shorten_chat_history(self):
+        self.recent_chat_history = self.__get_shorten_chat_history(self.recent_chat_history, self.settings.get_trace_back("max_length"))
+        return self.agent
+
+    def add_chat_history(self, role: str, content: str):
+        if role not in ("user", "assistant"):
+            raise Exception("[Agent Component]: Session - add_chat_history() only accept role type 'user' or 'assistant'.")
+        is_strict_orders = self.settings.get("strict_orders")
+        if is_strict_orders:
+            if len(self.full_chat_history) > 0:
+                if self.full_chat_history[-1]["role"] == role:
+                    self.full_chat_history[-1]["content"] += "\n" + content
+                    self.recent_chat_history[-1]["content"] += "\n" + content
+                else:
+                    self.full_chat_history.append({ "role": role, "content": content})
+                    self.recent_chat_history.append({ "role": role, "content": content})
+            else:
+                if role == "user":
+                    self.full_chat_history.append({ "role": role, "content": content})
+                    self.recent_chat_history.append({ "role": role, "content": content})
+                else:
+                    self.full_chat_history.extend([
+                        { "role": "user", "content": "[EMPTY]" },
+                        { "role": role, "content": content},
+                    ])
+                    self.recent_chat_history.extend([
+                        { "role": "user", "content": "[EMPTY]" },
+                        { "role": role, "content": content},
+                    ])
+        else:
+            self.full_chat_history.append({ "role": role, "content": content})
+            self.recent_chat_history.append({ "role": role, "content": content})
+        return self.agent
+
+    def get_chat_history(self, *, is_shorten: bool=False):
+        if is_shorten:
+            self.recent_chat_history = self.__get_shorten_chat_history(self.recent_chat_history, self.settings.get_trace_back("max_length"))
+            return self.recent_chat_history
+        else:
+            return self.full_chat_history
+
+    def rewrite_chat_history(self, new_chat_history: list):
+        self.full_chat_history = new_chat_history
+        self.recent_chat_history = new_chat_history
+        return self.agent
 
     def __find_input(self, input_data: any):
         if isinstance(input_data, str):
@@ -86,9 +138,14 @@ class Session(ComponentABC):
             return str(reply_data)
 
     def _prefix(self):
+        prefix_result = {}
         if self.current_session_id != None:
             self.shorten_chat_history()
-            return ("chat_history", self.recent_chat_history)
+            prefix_result.update({ "chat_history": self.recent_chat_history })
+        abstract = self.abstract.get()
+        if abstract:
+            prefix_result.update({ "abstract": abstract })
+        return prefix_result
 
     def _suffix(self, event: str, data: any):
         if self.current_session_id != None:
@@ -110,6 +167,11 @@ class Session(ComponentABC):
                 "stop_session": { "func": self.stop },
                 "toggle_session_auto_save": { "func": self.toggle_auto_save },
                 "set_chat_history_max_length": { "func": self.set_max_length },
+                "set_strict_orders": { "func": self.set_strict_orders },
+                "add_chat_history": { "func": self.add_chat_history },
+                "get_chat_history": { "func": self.get_chat_history, "return_value": True },
+                "rewrite_chat_history": { "func": self.rewrite_chat_history },
+                "set_abstract": { "func": self.abstract.assign },
             },
         }
 
