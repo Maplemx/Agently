a577f8c730db2313469857eca48cb0960aa56d68
Maplemx <Maplemx@gmail.com>
2024-02-27 10:14:50 +0800

    update: combine fix_json() methods

--- src/utils/load_json.py
+++ src/utils/load_json.py
@@ -25,35 +25,12 @@ def check_structure(origin: any, compare_target: any, position: str=""):
     else:
         return errors
 
-async def fix_json_structure(origin: str, input_dict: any, output_dict: any, request: object, *, is_debug: bool=False, errors = list):
+async def fix_json(origin: str, input_dict: any, output_dict: any, request: object, *, is_debug: bool=False, errors = list):
     try:
         fixed_result = await request\
             .input({
                 "error JSON String": origin ,
-                "error": "structure of {error JSON String} is not the same as {except structure}",
-                "error position": errors,
-            })\
-            .output('FIXED {error JSON String} JSON STRING ONLY WITHOUT EXPLANATION that can be parsed by Python')\
-            .start_async()
-        json_string = find_json(fixed_result)
-        if is_debug:
-            print("[Cleaned JSON String]:\n", json_string)
-            print("\n--------------------------\n")
-        fixed_result = json5.loads(json_string)
-        if is_debug:
-            print("[Parse JSON to Dict] Done")
-            print("\n--------------------------\n")
-        return fixed_result
-    except Exception as e:
-        raise Exception(f"[Agent Request] Error still occured when try to fix JSON decode error: { str(e) }\nOrigin JSON String:\n{ json_string }")
-
-async def fix_json_format(origin: str, input_dict: any, output_dict: any, request: object, *, is_debug: bool=False, error: str, position: str):
-    try:
-        fixed_result = await request\
-            .input({
-                "error JSON String": origin ,
-                "error": error,
-                "error position": position,
+                "error info": errors,
             })\
             .output('FIXED {error JSON String} JSON STRING ONLY WITHOUT EXPLANATION that can be parsed by Python')\
             .start_async()
@@ -77,14 +54,10 @@ async def load_json(origin: str, input_dict: any, output_dict: any, request: obj
             print("\n--------------------------\n")
         parsed_dict = json5.loads(json_string)
         return parsed_dict
-    except json.JSONDecodeError as e:
-        if is_debug:
-            print("[JSON Decode Error Occurred] Start Fixing Process...")
-        return await fix_json_format(json_string, input_dict, output_dict, request, is_debug = is_debug, error = e.msg, position = e.pos)
     except Exception as e:
         if is_debug:
             print("[JSON Decode Error Occurred] Start Fixing Process...")
-        return await fix_json_structure(origin, input_dict, output_dict, request, is_debug = is_debug, errors = [str(e)])
+        return await fix_json(origin, input_dict, output_dict, request, is_debug = is_debug, errors = [str(e)])
 
 def find_and_load_json(origin: str):
     try:
