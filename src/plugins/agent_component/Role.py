33384927a512893eb133d9c6017195858bdccc38
Maplemx <Maplemx@gmail.com>
2024-02-27 10:00:27 +0800

    update: change .set_role_name() to .set_role_id() and the role id will not be passed to agent request now

--- src/plugins/agent_component/Role.py
+++ src/plugins/agent_component/Role.py
@@ -7,8 +7,8 @@ class Role(ComponentABC):
         self.role_runtime_ctx = RuntimeCtxNamespace("role", self.agent.agent_runtime_ctx)
         self.role_storage = self.agent.global_storage.table("role")
 
-    def set_name(self, name: str):
-        self.role_runtime_ctx.set("NAME", name)
+    def set_id(self, role_id: str):
+        self.role_runtime_ctx.set("$ID", role_id)
         return self.agent
 
     def set(self, key: any, value: any=None):
@@ -39,26 +39,28 @@ class Role(ComponentABC):
             self.role_runtime_ctx.extend("DESC", key)
         return self.agent
 
-    def save(self, role_name: str=None):
-        if role_name == None:
-            role_name = self.role_runtime_ctx.get("NAME")
-        if role_name != None and role_name != "":
-            self.role_storage\
-                .set(role_name, self.role_runtime_ctx.get())\
-                .save()
-            return self.agent
-        else:
-            raise Exception("[Agent Component: Role] Role attr 'NAME' must be stated before save. Use .set_role_name() to specific that.")
-
-    def load(self, role_name: str):
-        role_data = self.role_storage.get(role_name)
+    def save(self, role_id: str=None):
+        role_id = role_id or self.role_runtime_ctx.get("$ID")
+        if role_id is None or role_id == "":
+            raise Exception("[Agent Component: Role] Role ID must be stated before save. Use .set_role_id() to specific it or pass role id into .save_role(<role_id>).")
+        self.role_storage.set(role_id, self.role_runtime_ctx.get()).save()
+        return self.agent
+            
+    def load(self, role_id: str):
+        role_id = role_id or self.role_runtime_ctx.get("$ID")
+        if role_id is None or role_id == "":
+            raise Exception("[Agent Component: Role] Role ID must be stated before save. Use .set_role_id() to specific it or pass role id into .load_role(<role_id>).")
+        role_data = self.role_storage.get(role_id)
         for key, value in role_data.items():
             self.role_runtime_ctx.update(key, value)
         return self.agent
 
     def _prefix(self):
+        role_settings = self.role_runtime_ctx.get() or {}
+        if "$ID" in role_settings:
+            del role_settings["$ID"]
         return {
-            "role": self.role_runtime_ctx.get(),
+            "role": role_settings,
         }
 
     def export(self):
@@ -66,7 +68,7 @@ class Role(ComponentABC):
             "prefix": self._prefix,
             "suffix": None,
             "alias": {
-                "set_role_name": { "func": self.set_name },
+                "set_role_id": { "func": self.set_id },
                 "set_role": { "func": self.set },
                 "update_role": { "func": self.update },
                 "append_role": { "func": self.append },
