5ab79396753998982155c69ee412f9f4c468bef1
Maplemx <Maplemx@gmail.com>
2024-06-13 17:30:14 +0800

    update: output inputs instead of inputs["default"] in end chunk to keep behavior unison

--- Agently/Workflow/executors/EndExecutor.py
+++ Agently/Workflow/executors/EndExecutor.py
@@ -2,5 +2,5 @@ from ..lib.Store import Store
 from ..lib.constants import WORKFLOW_END_DATA_HANDLE_NAME
 
 def end_executor(inputs, store: Store, **sys_info):
-    sys_info['sys_store'].set(WORKFLOW_END_DATA_HANDLE_NAME, inputs.get('default'))
+    sys_info['sys_store'].set(WORKFLOW_END_DATA_HANDLE_NAME, inputs)
     return inputs
