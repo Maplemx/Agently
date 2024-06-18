492ff7adeba9a16f01e63bf462e6fe797d6b4332
Maplemx <Maplemx@gmail.com>
2024-06-18 11:15:46 +0800

    update: be compatible with request messages those content is not a string or a standard multi-media content list

--- Agently/plugins/request/utils/format.py
+++ Agently/plugins/request/utils/format.py
@@ -5,6 +5,8 @@ def format_request_messages(request_messages, model_settings):
     role_list = ["user", "assistant"]
     current_role = 0
     for message in request_messages:
+        if "role" not in message or "content" not in message:
+            raise Exception(f"[Agently Request]: key 'role' and key 'content' must be contained in `request_messages`:\n{ str(request_messages) }")
         if message["role"] == "system":
             # no_multi_system_messages=True
             if model_settings.get_trace_back("message_rules.no_multi_system_messages"):
@@ -12,8 +14,12 @@ def format_request_messages(request_messages, model_settings):
                     system_prompt += f"{ message['content'] }\n"
                 elif isinstance(message["content"], list):
                     for content in message["content"]:
-                        if content["type"] == "text":
+                        if "type" in content and content["type"] == "text":
                             system_prompt += f"{ content['text'] }\n"
+                        else:
+                            system_prompt += f"{ str(content) }\n"
+                else:
+                    system_prompt += f"{ str(message['content']) }\n"
             # no_multi_system_messages=False
             else:
                 system_messages.append(message)
@@ -51,7 +57,11 @@ def format_request_messages(request_messages, model_settings):
             if isinstance(message["content"], str):
                 formatted_messages.append(message)
             elif isinstance(message["content"], list):
-                for item in message["content"]:
-                    if item["type"] == "text":
-                        formatted_messages.append({ "role": message["role"], "content": item["text"] })
+                for content in message["content"]:
+                    if "type" in content and content["type"] == "text":
+                        formatted_messages.append({ "role": message["role"], "content": content["text"] })
+                    else:
+                        formatted_messages.append({ "role": message["role"], "content": str(content) })
+            else:
+                formatted_messages.append({ "role": message["role"], "content": str(content) })
     return formatted_messages
