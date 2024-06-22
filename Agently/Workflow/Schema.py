c8384bbdfb361962433514340c60e2ece742a52a
Maplemx <Maplemx@gmail.com>
2024-06-22 11:33:17 +0800

    update: optimize chunk expression in error excpetion

--- Agently/Workflow/Schema.py
+++ Agently/Workflow/Schema.py
@@ -190,17 +190,17 @@ class Schema:
                 # 如果是非条件的情况，直接出报错，然后跳出查找
                 if not edge.get('condition') and not is_condition_connection:
                     status_res['status'] = 'error'
-                    status_res['message'] = f'An identical connection relationship "{source_view_name} → {target_view_name}" already exists.'
+                    status_res['message'] = f'An identical connection relationship [ {source_view_name} → {target_view_name} ] already exists.'
                     break
                 # 自身为条件连接的情况，直接出 warning，然后跳出查找
                 elif is_condition_connection:
                     status_res['status'] = 'warning'
-                    status_res['message'] = f'An identical connection relationship "{source_view_name} → {target_view_name}" already exists.'
+                    status_res['message'] = f'An identical connection relationship [ {source_view_name} → {target_view_name} ] already exists.'
                     break
                 # 先写入告警，但继续尝试看是否有报错的情况
                 else:
                     status_res['status'] = 'warning'
-                    status_res['message'] = f'An identical connection relationship "{source_view_name} → {target_view_name}" already exists.'
+                    status_res['message'] = f'An identical connection relationship [ {source_view_name} → {target_view_name} ] already exists.'
         return status_res
 
     
