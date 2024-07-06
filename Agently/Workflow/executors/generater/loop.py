1743d64ed4369bc11c99caf00fd707c100b8ffc4
Maplemx <Maplemx@gmail.com>
2024-07-06 23:15:20 +0800

    update: change output data format of .loop_with() and add compatible support

--- Agently/Workflow/executors/generater/loop.py
+++ Agently/Workflow/executors/generater/loop.py
@@ -20,20 +20,18 @@ def use_loop_executor(sub_workflow):
         elif isinstance(input_val, int):
             for i in range(input_val):
                 all_result.append(await loop_unit_core(unit_val=i, store=store))
-        #return all_result
-        # Regroup
-        final_result = {}
-        for item in all_result:
-            if item:
-                for key, value in item.items():
-                    if key not in final_result:
-                        final_result.update({ key: [] })
-                    final_result[key].append(value)
-        return (
-            final_result["default"]
-            if len(final_result.keys()) == 1 and "default" in final_result
-            else final_result
-        )
+        # Old Version Compatible
+        if sub_workflow.settings.get("compatible_version") and sub_workflow.settings.get("compatible_version") <= 3315:
+            return all_result
+        else:
+            # Regroup
+            final_result = {}
+            for one_result in all_result:
+                for handle, result in one_result.items():
+                    if handle not in final_result:
+                        final_result.update({ handle: [] })
+                    final_result[handle].append(result)
+            return final_result
 
     async def loop_unit_core(unit_val, store):
         if inspect.iscoroutinefunction(sub_workflow):
