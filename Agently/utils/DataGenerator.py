040f8f9251f497cab2ea6a51d855fd1cf9d81c74
Maplemx <Maplemx@gmail.com>
2024-07-26 22:36:20 +0800

    feat: DataGenerator

---
+++ Agently/utils/DataGenerator.py
@@ -0,0 +1,20 @@
+import queue
+
+class DataGenerator:
+    def __init__(self):
+        self.queue = queue.Queue()
+    
+    def generator(self):
+        while True:
+            data = self.queue.get()
+            if data == "$END$":
+                break
+            else:
+                yield data
+    
+    def add(self, data):
+        self.queue.put(data)
+        return self
+    
+    def end(self):
+        self.queue.put("$END$")
--- Agently/utils/__init__.py
+++ Agently/utils/__init__.py
@@ -8,3 +8,4 @@ from .DataOps import DataOps, NamespaceOps
 from .transform import to_prompt_structure, to_json_desc, to_instruction, find_all_jsons, find_json
 from .check_version import check_version
 from .load_json import load_json, find_and_load_json
+from .DataGenerator import DataGenerator
