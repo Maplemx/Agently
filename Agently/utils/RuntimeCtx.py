a1e6a75a84bcd2af8c0fb21df0cea71a25b5385f
Maplemx <Maplemx@gmail.com>
2024-11-14 20:33:08 +0800

    bug fix: fixed a bug that RuntimeCtx can not return values like 0, "", etc. correctly

--- Agently/utils/RuntimeCtx.py
+++ Agently/utils/RuntimeCtx.py
@@ -4,10 +4,10 @@ class RuntimeCtxNamespace(NamespaceOps):
     def __init__(self, namespace_name: str, runtime_ctx: object, *, return_to: object=None):
         super().__init__(namespace_name, runtime_ctx, return_to = return_to)
 
-    def get_trace_back(self, keys_with_dots: (str, None) = None, default: str=None):
+    def get_trace_back(self, keys_with_dots: str=None, default: str=None):
         return self.data_ops.get_trace_back(f"{ self.namespace_name }.{ keys_with_dots }" if keys_with_dots else self.namespace_name, default)
 
-    def get(self, keys_with_dots: (str, None) = None, default: str=None, *, trace_back = True):
+    def get(self, keys_with_dots: str=None, default: str=None, *, trace_back=True):
         if trace_back:
             return self.get_trace_back(keys_with_dots, default)
         else:
@@ -31,7 +31,7 @@ class RuntimeCtx(DataOps):
                 parent_result[key] = result[key]
         return parent_result
 
-    def get_trace_back(self, keys_with_dots: (str, None) = None, default: str=None):
+    def get_trace_back(self, keys_with_dots:str=None, default: str=None):
         result = self.get(keys_with_dots, trace_back = False)
         parent_result = self.parent.get_trace_back(keys_with_dots) if self.parent else None
         if result or parent_result:
@@ -39,11 +39,11 @@ class RuntimeCtx(DataOps):
                 parent_result = parent_result if isinstance(parent_result, dict) else {}
                 return self.__update_trace_back_result(parent_result, result)
             else:
-                return result if result else parent_result
+                return result if result is not None else parent_result
         else:
             return default
 
-    def get(self, keys_with_dots: (str, None) = None, default: str=None, *, trace_back = True):
+    def get(self, keys_with_dots: str=None, default: str=None, *, trace_back=True):
         if trace_back:
             return self.get_trace_back(keys_with_dots, default)
         else:
