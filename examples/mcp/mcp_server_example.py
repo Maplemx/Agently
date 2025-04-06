b6c084e5c914e73e996db821d1af87aa27a22d20
Maplemx <maplemx@gmail.com>
2025-04-06 16:38:56 +0800

    new example: MCP support

---
+++ examples/mcp/mcp_server_example.py
@@ -0,0 +1,27 @@
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+mcp_server = FastMCP("Cocktail Info")
+
+@mcp_server.prompt()
+async def look_up_cocktail_info_prompt(message: str) -> str:
+    return f"Please check cocktail information in '{ message }'. Cocktail name MUST BE translated into English!"
+
+@mcp_server.tool()
+async def look_up_cocktail_info(cocktail_name:str):
+    """Look up cocktail information by name.
+The name MUST BE translated into English and the name MUST BE the basic name of cocktail.
+For Example:
+'Margarita' is RIGHT!
+'Spicy Mango Margarita' is WRONG!"""
+    return (
+        httpx.get(
+            "https://www.thecocktaildb.com/api/json/v1/1/search.php",
+            params={
+                "s": cocktail_name,
+            }
+        )
+        .content.decode()
+    )
+
+mcp_server.run()
