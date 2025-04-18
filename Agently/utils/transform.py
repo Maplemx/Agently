578d1b30be5bc7c074e4572843f8521a2f7abb61
Maplemx <maplemx@gmail.com>
2025-04-18 17:30:42 +0800

    update: optimize JSON description generation code

--- Agently/utils/transform.py
+++ Agently/utils/transform.py
@@ -24,40 +24,89 @@ def to_instruction(origin):
     else:
         return str(origin)
 
-def to_json_desc(origin, layer_count = 0):
+# def to_json_desc(origin, layer_count = 0):
+#     if isinstance(origin, dict):
+#         json_string = ""
+#         if layer_count > 0:
+#             json_string += "\n"
+#         json_string += ("\t" * layer_count) + "{\n"
+#         for key, value in origin.items():
+#             json_string += ("\t" * (layer_count + 1)) + "\"" + key + "\": " + to_json_desc(value, layer_count + 1) + "\n"
+#         if layer_count > 0:
+#             json_string += ("\t" * (layer_count + 1)) + "},"
+#         else:
+#             json_string += "}"
+#         return json_string
+#     elif isinstance(origin, (list, set)):
+#         json_string = ""
+#         if layer_count > 0:
+#             json_string += "\n"
+#         json_string += ("\t" * layer_count) + "[\n"
+#         for item in origin:
+#             json_string += ("\t" * (layer_count + 1)) + to_json_desc(item, layer_count + 1) + ",\n"
+#         json_string += ("\t" * (layer_count + 1)) + "...\n"
+#         if layer_count > 0:
+#             json_string += ("\t" * layer_count) + "],"
+#         else:
+#             json_string += "]"
+#         return json_string
+#     elif isinstance(origin, tuple):
+#         if isinstance(origin[0], (dict, list, set)):
+#             json_string = f"\n{ to_json_desc(origin[0], layer_count + 1) },"
+#         else:
+#             if len(origin) >= 3 and origin[2]:
+#                 json_string += f"[*Required] "
+#             json_string = f"<{ str(origin[0]) }>,"
+#         if len(origin) >= 2:
+#             json_string += f"//{ str(origin[1]) }"
+#         return json_string
+#     else:
+#         return str(origin)
+
+def to_json_desc(origin, layer_count=0, in_list=False):
+    indent = "\t" * layer_count
+    next_indent = "\t" * (layer_count + 1)
+
     if isinstance(origin, dict):
-        json_string = ""
-        if layer_count > 0:
-            json_string += "\n"
-        json_string += ("\t" * layer_count) + "{\n"
-        for key, value in origin.items():
-            json_string += ("\t" * (layer_count + 1)) + "\"" + key + "\": " + to_json_desc(value, layer_count + 1) + "\n"
-        if layer_count > 0:
-            json_string += ("\t" * (layer_count + 1)) + "},"
-        else:
-            json_string += "}"
-        return json_string
+        lines = ["{"]
+        items = list(origin.items())
+        for i, (key, value) in enumerate(items):
+            val_str = to_json_desc(value, layer_count + 1)
+            line = f'{next_indent}"{key}": {val_str}'
+            if i < len(items) - 1:
+                line += ","
+            lines.append(line)
+        lines.append(indent + "}" + ("," if in_list else ""))
+        return "\n".join(lines)
+
     elif isinstance(origin, (list, set)):
-        json_string = ""
-        if layer_count > 0:
-            json_string += "\n"
-        json_string += ("\t" * layer_count) + "[\n"
-        for item in origin:
-            json_string += ("\t" * (layer_count + 1)) + to_json_desc(item, layer_count + 1) + ",\n"
-        json_string += ("\t" * (layer_count + 1)) + "...\n"
-        if layer_count > 0:
-            json_string += ("\t" * layer_count) + "],"
-        else:
-            json_string += "]"
-        return json_string
+        lines = ["["]
+        items = list(origin)
+        for i, item in enumerate(items):
+            line = to_json_desc(item, layer_count + 1)
+            if i < len(items) - 1:
+                line += ","
+            lines.append(next_indent + line)
+        lines.append(next_indent + "...")
+        lines.append(indent + "]")
+        return "\n".join(lines)
+
     elif isinstance(origin, tuple):
         if isinstance(origin[0], (dict, list, set)):
-            json_string = f"\n{ to_json_desc(origin[0], layer_count + 1) },"
+            result = to_json_desc(origin[0], layer_count)
         else:
-            json_string = f"<{ str(origin[0]) }>,"
-        if len(origin) >= 2:
-            json_string += f"//{ str(origin[1]) }"
-        return json_string
+            desc = ""
+            if len(origin) >= 3 and origin[2]:
+                desc += "[*Required] "
+            desc += f"<{origin[0]}>"
+            if len(origin) >= 2:
+                desc += f"  // {origin[1]}"
+            result = desc
+        return result
+
+    elif isinstance(origin, str):
+        return f'"{ origin }"'
+
     else:
         return str(origin)
 
