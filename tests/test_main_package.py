4579847fa671b02d27953c010a0a69fbd396e804
Maplemx <maplemx@gmail.com>
2025-08-13 17:51:01 +0800

    clean up for v4.0.0-beta-2

--- tests/test_main_package.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from agently import Agently
-
-
-@pytest.mark.asyncio
-async def test_settings():
-    Agently.set_settings("test", "test")
-    assert Agently.settings["test"] == "test"
