0645e1dd0a07260eedbcbe8d61df4e5f9e71ebbc
Maplemx <maplemx@gmail.com>
2025-09-13 16:35:15 +0800

    update: loop until

--- examples/trigger_flow/loop.py
+++ examples/trigger_flow/loop.py
@@ -1,13 +1,33 @@
-from agently import TriggerFlow, TriggerFlowEventData
+from agently import Agently, TriggerFlow
 
 flow = TriggerFlow()
 
 
 @flow.chunk
-async def user_input(data: TriggerFlowEventData):
+async def user_input(_):
     return input("Say 'STOP' to exit: ")
 
 
-(flow.to(user_input).match().case("STOP").end().else_case().to(user_input).end_match())
+(
+    flow.to(user_input)
+    .____()
+    .match()
+    .case("STOP")
+    .to(lambda _: print("👋 bye~"))
+    .to(lambda data: data.get_runtime_data("results"))
+    .end()
+    .case_else()
+    .to(
+        lambda data: data.append_runtime_data(
+            "results",
+            data.value,
+            emit=False,
+        )
+    )
+    .to(user_input)
+    .end_match()
+    .____()
+)
 
-flow.start()
+results = flow.start()
+print(results)
