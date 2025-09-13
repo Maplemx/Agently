efd637c02bbc4bc7592584db0f8cbc20abc5b5e4
Maplemx <maplemx@gmail.com>
2025-09-13 16:31:51 +0800

    1. add .when()...;\n2. use avoid copy EMTPY

--- agently/core/TriggerFlow/process/BaseProcess.py
+++ agently/core/TriggerFlow/process/BaseProcess.py
@@ -20,10 +20,11 @@ from typing import Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from agently.core.TriggerFlow import TriggerFlowBluePrint
-    from agently.types.trigger_flow import TriggerFlowHandler, TriggerFlowEventData, TriggerFlowBlockData
+    from agently.types.trigger_flow import TriggerFlowHandler, TriggerFlowEventData
 
 from agently.core.TriggerFlow.Chunk import TriggerFlowChunk
-from agently.types.trigger_flow import EMPTY_RESULT
+from agently.types.data import EMPTY
+from agently.types.trigger_flow import TriggerFlowBlockData
 
 
 class TriggerFlowBaseProcess:
@@ -52,10 +53,35 @@ class TriggerFlowBaseProcess:
     ):
         return type(self)(
             trigger_event=trigger_event,
+            trigger_type=trigger_type,
             blue_print=blue_print,
             block_data=block_data,
             **options,
         )
+
+    def when(
+        self,
+        target: str,
+        *,
+        type: Literal["event", "runtime_data", "flow_data"] = "event",
+    ):
+        return self._new(
+            trigger_event=target,
+            trigger_type=type,
+            blue_print=self._blue_print,
+            block_data=TriggerFlowBlockData(
+                outer_block=None,
+            ),
+        )
+
+    def when_event(self, event: str):
+        return self.when(event, type="event")
+
+    def when_runtime_data(self, key: str):
+        return self.when(key, type="runtime_data")
+
+    def when_flow_data(self, key: str):
+        return self.when(key, type="flow_data")
 
     def to(self, chunk: "TriggerFlowChunk | TriggerFlowHandler | str", side_branch: bool = False):
         if isinstance(chunk, str):
@@ -117,7 +143,8 @@ class TriggerFlowBaseProcess:
 
     def end(self):
         async def set_default_result(data: "TriggerFlowEventData"):
-            if data.get_runtime_data("$TF.result") is EMPTY_RESULT:
+            result = data.get_runtime_data("$TF.result")
+            if result is EMPTY:
                 data.set_runtime_data("$TF.result", data.value)
             data.get_runtime_data("$TF.result_ready").set()
 
