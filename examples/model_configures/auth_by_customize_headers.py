10019413a034ee32167c5cd021bbf32205e9a85a
Maplemx <maplemx@gmail.com>
2025-10-21 11:23:39 +0800

    add: Customized authorization in headers and body

---
+++ examples/model_configures/auth_by_customize_headers.py
@@ -0,0 +1,46 @@
+import asyncio
+from agently import Agently
+
+Agently.set_settings(
+    "OpenAICompatible",
+    {
+        "base_url": "https://my-customize-server/v1",
+        "model": "my-model",
+        "auth": {
+            "headers": {
+                "Authorization": "Customize <My-Customize-Token>",
+            }
+        },
+        "options": {
+            "temperature": 0.7,
+        },
+    },
+)
+
+llm = Agently.create_agent()
+
+
+async def main():
+    instant_mode_response = (
+        llm.input("Give me 5 computer-related words and 3 color-related phrases and 1 random sentence.")
+        .output(
+            {
+                "words": [(str,)],
+                "phrases": {
+                    "<color-name>": (str, "phrase"),
+                },
+                "sentence": (str,),
+            }
+        )
+        .get_async_generator(content="instant")
+    )
+
+    async for event in instant_mode_response:
+        print(
+            event.path,
+            "[DONE]" if event.is_complete else "[>>>>]",
+            event.value,
+        )
+
+
+asyncio.run(main())
