42e0e9ac516811f258b8466ce9c0a4efce5fb444
Maplemx <maplemx@gmail.com>
2025-11-19 12:11:55 +0800

    add: Gemini

---
+++ examples/model_configures/Gemini.py
@@ -0,0 +1,42 @@
+import asyncio
+from agently import Agently
+
+Agently.set_settings(
+    "OpenAICompatible",
+    {
+        "base_url": "https://generativelanguage.googleapis.com/v1beta/openai/",
+        "model": "gemini-2.5-flash",
+        "auth": "<Google-AIStudio-API-Key>",
+        "options": {
+            "temperature": 0.7,
+        },
+    },
+)
+
+llm = Agently.create_agent()
+
+
+async def main():
+    instant_mode_response = (
+        llm.input("Give me 5 computer-related words and 3 color-related phrases and 1 random sentence.")
+        .output(
+            {
+                "words": [(str,)],
+                "phrases": {
+                    "<color-name>": (str, "phrase"),
+                },
+                "sentence": (str,),
+            }
+        )
+        .get_async_generator(type="instant")
+    )
+
+    async for event in instant_mode_response:
+        print(
+            event.path,
+            "[DONE]" if event.is_complete else "[>>>>]",
+            event.value,
+        )
+
+
+asyncio.run(main())
