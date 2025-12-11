b0cd450b0b10c76566137fbda2be16146c0d78f3
Maplemx <maplemx@gmail.com>
2025-12-11 16:22:51 +0800

    new: From agent prompt data to YAML-Prompt and JSON-Prompt

---
+++ examples/configure_prompt/to_configure_prompt.py
@@ -0,0 +1,55 @@
+from agently import Agently
+
+agent = Agently.create_agent()
+
+(
+    agent.role(
+        "You are an Agently enhanced agent.",
+        always=True,
+    )
+    .info(
+        {
+            "Agently": "Speed up your AI application development. Official website: https://Agently.tech.",
+        },
+        always=True,
+    )
+    .input("Say hello.")
+    .instruct(
+        [
+            "Reply {input} politely.",
+        ]
+    )
+    .output(
+        {
+            "thinking": (
+                [(str, "one step of plan")],
+                "plans to response",
+            ),
+            "reply": (str,),
+            "extra": (
+                {
+                    "worth_to_remember": (
+                        bool,
+                        "is {input} and {reply} worth to be remembered that not a normal daily chat?",
+                    ),
+                    "user_emotion_guess": (str, "how do you thinking user's emotion is going to be after {reply}?"),
+                },
+                "extra info you need to collect and analysis",
+            ),
+        }
+    )
+)
+
+yaml_prompt = agent.to_yaml_prompt()
+json_prompt = agent.to_json_prompt()
+
+print("[YAML PROMPT]:")
+print(yaml_prompt)
+print("[JSON PROMPT]:")
+print(json_prompt)
+
+agent_2 = Agently.create_agent()
+
+agent_2.load_yaml_prompt(yaml_prompt)
+print("[AGENT 2 PROMPT]:")
+print(agent_2.prompt.to_text())
